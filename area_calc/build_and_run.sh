0a03db2d78efc817639c4d60f294afbe3ee72a65
Oliver Blanthorn <freedom4cows@gmail.com>
2020-08-16 17:25:27 +0100

    Add classic area calculator

---
+++ area_calc/build_and_run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+clang -lstdc++ -Wall main.cpp -o main.o
+./main.o
---
+++ area_calc/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter width: ";
+    int x, y;
+    std::cin >> x;
+    std::cout << "Enter height: ";
+    std::cin >> y;
+    std::cout << "The area is: " << x*y << "\n";
+    return 0;
+}
